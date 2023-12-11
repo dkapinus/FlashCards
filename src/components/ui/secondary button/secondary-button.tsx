dda9ad86aa53627bcce260d2d2bfe2ee2387ee05
Sasha <skalitsenia@mail.ru>
2023-12-11 22:13:32 +0300

    Create Secondary Button

---
+++ src/components/ui/secondary button/secondary-button.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import s from './secondary-button.module.scss'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  className?: string
+  fullWidth?: boolean
+  variant?: 'active' | 'default' | 'disabled' | 'focus' | 'hover' | 'secondary'
+} & ComponentPropsWithoutRef<T>
+
+export const SecondaryButton = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => {
+  const { as: Component = 'button', className, fullWidth, variant = 'secondary', ...rest } = props
+
+  return (
+    <Component
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+    />
+  )
+}
