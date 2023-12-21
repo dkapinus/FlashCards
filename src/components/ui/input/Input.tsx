4a0eaccd27028468f6c5c8f7e6987f9e22672ee9
Sasha <skalitsenia@mail.ru>
2023-12-21 18:43:29 +0300

    Refactor

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -7,13 +7,13 @@ import s from './Input.module.scss'
 
 export type InputProps<T extends ElementType = 'input'> = {
   as?: T
-  className?: string
-  cross: boolean
-  error: boolean
-  eye: boolean
-  fullWidth?: boolean
-  magnifier: boolean
+  cross?: boolean
+  error?: boolean
+  eye?: boolean
+  label?: string
+  magnifier?: boolean
   onChange: (e: string) => void
+  value: string
   variant?: 'input'
 } & ComponentPropsWithoutRef<T>
 
@@ -23,20 +23,20 @@ export const Input = <T extends ElementType = 'input'>(
   const {
     as: Component = 'input',
     className,
-    cross,
-    error,
-    eye,
-    handleBlur,
+    cross = false,
+    error = false,
+    eye = false,
     label,
-    magnifier,
+    magnifier = false,
     onChange,
     value,
     variant = 'input',
     ...rest
   } = props
   const [touched, setTouched] = useState(false)
-
+  const [changeValue, setChangeValue] = useState(value)
   const onchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setChangeValue(e.currentTarget.value)
     onChange(e.currentTarget.value)
   }
 
@@ -53,10 +53,11 @@ export const Input = <T extends ElementType = 'input'>(
           <Icon height={'20'} iconId={'magnifier'} viewBox={'0 0 20 20'} width={'20'} />
         )}
         <Component
-          className={`${s.input} ${s[variant]} ${error ? s.errorText : ''} ${className}`}
+          className={`${s.input} ${s[variant]} ${error ? s.errorText : ''}`}
           onBlur={() => setTouched(false)}
           onChange={onchangeHandler}
           onClick={() => setTouched(true)}
+          value={changeValue}
           {...rest}
         />
         {eye && <Icon height={'20'} iconId={'eye'} viewBox={'0 0 20 20'} width={'20'} />}
