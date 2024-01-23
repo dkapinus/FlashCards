b637b0932a3cb66f1bda27c66586f43cd91922d9
AndreiDudenkov <andrei634d@gmail.com>
2024-01-23 03:02:29 +0300

    change cover in decks and in pack

--- src/components/ui/dropDown menu/dropDown-options/DropDown-Menu-Options.tsx
+++ src/components/ui/dropDown menu/dropDown-options/DropDown-Menu-Options.tsx
@@ -1,16 +1,30 @@
-import { ComponentPropsWithoutRef, ElementType, ReactElement } from 'react'
+import {
+  ChangeEvent,
+  ComponentPropsWithoutRef,
+  ElementType,
+  ReactElement,
+  useRef,
+  useState,
+} from 'react'
 
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Icon } from '@/components/ui/icon/Icon'
+import { Input } from '@/components/ui/input'
 import { ModalsForOptions } from '@/components/ui/modals/modalsForOptoins/ModalsForOptions'
 import { Typography } from '@/components/ui/typography'
+import { useUpdateDecksMutation } from '@/services/decks_Api/Decks.service'
+import { GetDeckById } from '@/services/decks_Api/Decks.types'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import d from './DropDown-Menu-Options.module.scss'
+import s from '@/pages/pack/Pack.module.scss'
 
 export type DropDownMenuOptionsProps<T extends ElementType = 'button'> = {
   as?: T
   child: ReactElement
   className?: string
+  deck: GetDeckById | undefined
   edit: (event: Event) => void
   learn: () => void
   mail?: string
@@ -26,6 +40,7 @@ export const DropDownMenuOptions = <T extends ElementType = 'button'>(
     as: Component = 'button',
     child,
     className,
+    deck,
     edit,
     learn,
     mail,
@@ -33,6 +48,54 @@ export const DropDownMenuOptions = <T extends ElementType = 'button'>(
     userName,
     ...rest
   } = props
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const selectFileHandler = () => {
+    inputRef && inputRef.current?.click()
+  }
+  const [updatePack] = useUpdateDecksMutation({})
+
+  const [isPrivatePack, setIsPrivatePack] = useState<boolean | undefined>(deck?.isPrivate)
+
+  const [packName, setPackName] = useState('')
+  const [photo, setPhoto] = useState<File>()
+
+  const onUploadPhoto = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files![0]
+
+    setPhoto(file)
+  }
+  const onChangeNamePack = (value: string) => {
+    setPackName(value)
+  }
+  const onclickPrivatePack = () => {
+    setIsPrivatePack(!isPrivatePack)
+    console.log(`isPrivatePack after onclick: ${isPrivatePack}`)
+  }
+
+  const UpDatePack = (id: string, deckName: string) => {
+    const formData = new FormData()
+
+    if (photo) {
+      formData.append('cover', photo)
+    }
+    // if (isPrivatePack) {
+    //   formData.append('isPrivate', String(isPrivatePack))
+    // }
+    //
+    // formData.append('name', deckName)
+    //
+    // updatePack(formData)
+
+    updatePack({
+      cover: formData,
+      id: id,
+      isPrivate: isPrivatePack,
+      name: packName === '' ? deckName : packName,
+    })
+    console.log(formData)
+    console.log(`isPrivatePack for server: ${isPrivatePack}`)
+  }
 
   return (
     <DropdownMenu.Root {...rest}>
@@ -59,9 +122,55 @@ export const DropDownMenuOptions = <T extends ElementType = 'button'>(
                   <Typography variant={'caption'}>Edit</Typography>
                 </div>
               }
+              buttonsInFooter={[
+                <Button onClick={() => {}} variant={'secondary'}>
+                  Cancel
+                </Button>,
+                <Button
+                  onClick={() => {
+                    if (deck) {
+                      UpDatePack(deck.id, deck.name)
+                    }
+                  }}
+                  variant={'primary'}
+                >
+                  Save changes
+                </Button>,
+              ]}
               modalTitle={'Edit Pack'}
               showCloseButton
-            ></ModalsForOptions>
+            >
+              <img alt={''} className={s.createPhoto} src={deck?.cover} />
+              <div>
+                <Button fullWidth onClick={selectFileHandler} variant={'secondary'}>
+                  Change cover
+                </Button>
+                <div>
+                  <Input
+                    onChange={onUploadPhoto}
+                    ref={inputRef}
+                    style={{ display: 'none', height: '0' }}
+                    type={'file'}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Input
+                  defaultValue={deck?.name}
+                  label={'Name Pack'}
+                  onValueChange={onChangeNamePack}
+                />
+              </div>
+
+              <div>
+                <Checkbox
+                  defaultChecked={deck?.isPrivate}
+                  label={'Private pack'}
+                  onClick={onclickPrivatePack}
+                />
+              </div>
+            </ModalsForOptions>
           </DropdownMenu.Item>
           <hr className={d.Line} />
           <DropdownMenu.Item className={d.DropdownMenuItem} onClick={remove}>
